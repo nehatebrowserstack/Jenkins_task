78977f800758f64bafabdfd66ae90862209d4cb5
nehatebrowserstack <neha.te@browserstack.com>
2026-01-19 14:17:50 +0530

    Update1

--- test/simple_test.rb
+++ test/simple_test.rb
@@ -6,34 +6,34 @@ require "selenium-webdriver"
 project_root = File.expand_path("..", __dir__)
 reports_path = File.join(project_root, "reports")
 
-
+# 2. Clean and create the folder at the absolute project root
 FileUtils.rm_rf(reports_path)
 FileUtils.mkdir_p(reports_path)
 
-
+# 3. Tell the reporter exactly where to go
 Minitest::Reporters.use!(
-  Minitest::Reporters::JUnitReporter.new(reports_path, single_file: true)
+  Minitest::Reporters::JUnitReporter.new(reports_path)
 )
-
 class BrowserStackTest < Minitest::Test
   def test_example_page
     username = ENV["BROWSERSTACK_USERNAME"]
     access_key = ENV["BROWSERSTACK_ACCESS_KEY"]
 
-    caps = Selenium::WebDriver::Remote::Capabilities.chrome(
-      "bstack:options" => {
-        userName: username,
-        accessKey: access_key,
-        sessionName: "Simple Rake Test",
-        os: "Windows",
-        osVersion: "10"
-      }
-    )
+    # Chrome options object
+    options = Selenium::WebDriver::Chrome::Options.new
+    options.add_option("bstack:options", {
+      userName: username,
+      accessKey: access_key,
+      sessionName: "Simple Rake Test",
+      os: "Windows",
+      osVersion: "10"
+    })
 
+    # Remote driver
     driver = Selenium::WebDriver.for(
       :remote,
       url: "https://hub.browserstack.com/wd/hub",
-      capabilities: caps
+      capabilities: options
     )
 
     driver.navigate.to("https://example.com")
